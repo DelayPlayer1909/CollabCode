86a47946202949c5674e01b7077111f222cd4b0c
DelayPlayer1909 <cshekhar.p100@gmail.com>
2025-05-06 23:12:40 +0530

    Update Sidebar.tsx file

--- client/src/components/sidebar/Sidebar.tsx
+++ client/src/components/sidebar/Sidebar.tsx
@@ -1,18 +1,12 @@
 import SidebarButton from "@/components/sidebar/sidebar-views/SidebarButton"
-import { useAppContext } from "@/context/AppContext"
-import { useSocket } from "@/context/SocketContext"
+
 import { useViews } from "@/context/ViewContext"
 import useResponsive from "@/hooks/useResponsive"
-import useWindowDimensions from "@/hooks/useWindowDimensions"
-import { ACTIVITY_STATE } from "@/types/app"
-import { SocketEvent } from "@/types/socket"
+
 import { VIEWS } from "@/types/view"
-import { IoCodeSlash } from "react-icons/io5"
-import { MdOutlineDraw } from "react-icons/md"
+
 import cn from "classnames"
-import { Tooltip } from 'react-tooltip'
-import { useState } from 'react'
-import { tooltipStyles } from "./tooltipStyles"
+
 
 function Sidebar() {
     const {
@@ -20,27 +14,13 @@ function Sidebar() {
         isSidebarOpen,
         viewComponents,
         viewIcons,
-        setIsSidebarOpen,
     } = useViews()
     const { minHeightReached } = useResponsive()
-    const { activityState, setActivityState } = useAppContext()
-    const { socket } = useSocket()
-    const { isMobile } = useWindowDimensions()
-    const [showTooltip, setShowTooltip] = useState(true)
 
-    const changeState = () => {
-        setShowTooltip(false)
-        if (activityState === ACTIVITY_STATE.CODING) {
-            setActivityState(ACTIVITY_STATE.DRAWING)
-            socket.emit(SocketEvent.REQUEST_DRAWING)
-        } else {
-            setActivityState(ACTIVITY_STATE.CODING)
-        }
 
-        if (isMobile) {
-            setIsSidebarOpen(false)
-        }
-    }
+
+
+
 
     return (
         <aside className="flex w-full md:h-full md:max-h-full md:min-h-full md:w-auto">
@@ -74,21 +54,7 @@ function Sidebar() {
                 />
 
                 {/* Button to change activity state coding or drawing */}
-                <div className="flex items-center justify-center h-fit">
-                    
-                    {showTooltip && (
-                        <Tooltip 
-                            id="activity-state-tooltip"
-                            place="right"
-                            offset={15}
-                            className="!z-50"
-                            style={tooltipStyles}
-                            noArrow={false}
-                            positionStrategy="fixed"
-                            float={true}
-                        />
-                    )}
-                </div>
+                
             </div>
             <div
                 className="absolute left-0 top-0 z-20 w-full flex-col bg-dark md:static md:min-w-[300px]"
